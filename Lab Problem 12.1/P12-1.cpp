def8397ceb861c785b161bba6c6a6aca33a9bcc3
Caleb Collert <c-collert.1@onu.edu>
2024-05-01 23:27:46 -0400

    character count finished

--- Lab Problem 12.1/P12-1.cpp
+++ Lab Problem 12.1/P12-1.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int mian() {
+void pressEnterToContinue();
+int characterCount(const string& fileName);
 
+int main() {
+    int charCount;
+    string datafile;
+    cout << "Enter input file: " << endl;
+    getline(cin, datafile);
+    cout << "Characters: " << characterCount(datafile) << endl;
+    pressEnterToContinue();
+}
+
+void pressEnterToContinue() {
+    cout << "\nPress enter to continue...\n";
+    cin.clear();
+    while (cin.get() != '\n') continue;
+}
+
+int characterCount(const string& fileName) {
+    ifstream inFile(fileName);
+    char characters;
+    int charCount = 0;
+
+    if (!inFile.is_open()) {
+        cout << "Failed to open file." << endl;
+        return -1;
+    }
+
+    while (inFile.get(characters)) {
+        if (characters != ' ' && characters != '\n') {
+            charCount++;
+        }
+    }
+
+    inFile.close();
+    return charCount;
 }
